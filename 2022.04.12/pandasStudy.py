5b2146b16295f41ae2c96350b3e9cc3ce9d79be9
S_hine2 <rhdk5148@gmail.com>
2022-04-13 14:52:02 +0900

    Update pandasStudy.py

--- 2022.04.12/pandasStudy.py
+++ 2022.04.12/pandasStudy.py
@@ -1,12 +1,16 @@
+import pandas as pd
+
 def avg(list1):
     avg = [n/5 for n in list1]
     return avg
+
 df = pd.DataFrame({"이름":['서준','우현','인아'],
                   '수학':[90,80,70],
                   '영어':[98,89,95],
                   '음악':[85,95,100],
                   '체육':[100,90,90],
                   '국어':[80,80,80]})
+
 sumAll = []
 for i in range(3):
     sum = 0
@@ -14,6 +18,12 @@ for i in range(3):
         if type(j) != type("a"):
             sum += j
     sumAll.append(sum)
+    
 df['총점'] = sumAll
 df['평균'] = avg(sumAll)
-print(df)
+
+print(df.set_index('이름'))
+
+df.to_excel('./to_excel_20183343.xlsx',index=False)
+df.to_csv('./to_csv_20183343.csv',index=False, encoding = 'euc-kr')
+df.to_json('./to_json_20183343.json')
