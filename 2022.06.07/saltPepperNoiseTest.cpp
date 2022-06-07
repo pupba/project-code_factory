aabd926103201d98311b46eb39a9467ba8ca27aa
S_hine2 <rhdk5148@gmail.com>
2022-06-08 00:22:24 +0900

    Create saltPepperNoiseTest.cpp

---
+++ 2022.06.07/saltPepperNoiseTest.cpp
@@ -0,0 +1,31 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+Mat saltPepper(Mat org, float percent) {
+	Mat dst;
+	org.copyTo(dst);
+
+	int nNoise = (int)(dst.cols * dst.rows * percent);
+	for (int i = 0; i < nNoise; i++) {
+		int c = rand() % dst.cols;
+		int r = rand() % dst.rows;
+		dst.at<uchar>(r, c) = (i % 2) * 255;
+	}
+	return dst;
+}
+
+int main()
+{
+	Mat src = imread("./img/cat1.jpg",IMREAD_GRAYSCALE);
+	resize(src, src, Size(500, 500));
+	imshow("Origin", src);
+
+	Mat dst = saltPepper(src, 0.1);
+
+	imshow("Salt Pepper 10% Noise", dst);
+
+	waitKey(0);
+	return 0;
+}
